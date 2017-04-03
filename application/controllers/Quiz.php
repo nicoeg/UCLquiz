81784211d773bc82a49cd707a5329d30113bd556
Lasse Lentz Thomsen <kontakt@lasselentz.dk>
2017-04-03 14:45:00 +0200

    Add some functionality to quiz

---
+++ application/controllers/Quiz.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Quiz extends CI_Controller
+{
+	public function __construct()
+	{
+		Parent::__construct();
+		$this->load->model('Quiz_model');
+	}
+	
+	public function index()
+	{
+		
+	}
+}
