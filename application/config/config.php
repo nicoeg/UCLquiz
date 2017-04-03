c51c5019fcbcdd6231b17e788449f744a1ea3b77
Nicolaj Egelund <nicomanden@gmail.com>
2017-04-03 12:19:43 +0200

    Updated to fix php error in other enviroment

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = getenv('BASE_URL', 'http://'.$_SERVER['SERVER_NAME'].'/UCLquiz');
+$config['base_url'] = getenv('BASE_URL') ?: 'http://'.$_SERVER['SERVER_NAME'].'/UCLquiz';
 
 /*
 |--------------------------------------------------------------------------
