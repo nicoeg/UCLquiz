047bc776ae7889253d5455267e921dba834a1d9d
Nicolaj Egelund <nicomanden@gmail.com>
2017-04-03 12:27:03 +0200

    Added grid

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="stylesheet" href="<?= base_url('style/flexboxgrid.min.css'); ?>">
     <link rel="stylesheet" href="<?= base_url('style/stylesheet.css'); ?>">
 	<title>UCLquiz</title>
 </head>
