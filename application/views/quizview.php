2ea57b50a863acde0822a7810d35e405b023d3b9
JRJLPU <JRJLPU@gmail.com>
2017-04-03 13:31:30 +0200

    quiz

---
+++ application/views/quizview.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row-center-xs">
+        <div class="col-xs-6">
+            <table style="width: 50%">
+                <tr>
+                    <th>
+                        <p>Quiz</p>
+                    </th>
+                    <th>
+                        <p>Fag</p>
+                    </th>
+                    <th>
+                        <p>Udgivet af</p>
+                    </th>
+                    <th>
+                        Niveau
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Data</p>
+                    </td>
+                    <td>
+                        <p>Data</p>
+                    </td>
+                    <td>
+                        <p>Data</p>
+                    </td>
+                    <td>
+                        <p>Data</p>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
