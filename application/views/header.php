3918e774082568cb1d681eac16ceea4f5d60b494
Nicolaj Egelund <nicomanden@gmail.com>
2017-04-03 11:45:11 +0200

    Added basic html

---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+    <link rel="stylesheet" href="/style/stylesheet.css">
+	<title>UCLquiz</title>
+</head>
+<body>
