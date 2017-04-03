ae0c73d62e5cf3a737d8d84e7a3ceac4bba21e77
Nicolaj Egelund <nicomanden@gmail.com>
2017-04-03 13:37:12 +0200

    Added login container

--- application/views/login.php
+++ application/views/login.php
@@ -1,3 +1,6 @@
-<div class="container">
-
+<div class="row center-lg center-md center-sm">
+    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6 login-container">
+        <form action="" method="POST"></form>
+        <img class="quote" src="<?= base_url('images/quote.png') ?>" alt="Quote">
+    </div>
 </div>
