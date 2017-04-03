65c4611c17c7ac7cdd910e0e7d088337849b5958
Lasse Lentz Thomsen <kontakt@lasselentz.dk>
2017-04-03 13:41:34 +0200

    Add user model

--- application/models/User_Model.php
+++ application/models/User_Model.php
@@ -2,5 +2,15 @@
 
 class User_Model extends CI_Model
 {
-	
+	public function getUserByEmail(string $email)
+	{
+		$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+		$query = $this->db
+			->get('users')
+			->where('email', $email)
+			->limit(1);
+
+		return $query->row();
+	}
 }
