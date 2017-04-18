730ac4fd28677d1f532eb3d4653301cd52d82f2f
Lasse Lentz Thomsen <kontakt@lasselentz.dk>
2017-04-18 17:07:30 +0200

    Naming conventions + refactor

--- application/controllers/Quiz_View.php
+++ application/controllers/Quiz_View.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class QuizView extends CI_Controller {
+class Quiz_View extends CI_Controller {
     public function index() {
         $this->load->model('Quiz_Model', 'quizModel');
 
