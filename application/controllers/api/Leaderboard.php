fd142cb48289d52fcd8cd81cd2a19b8d1390f995
Lasse Lentz Thomsen <kontakt@lasselentz.dk>
2017-05-09 13:18:36 +0200

    Fix for getleaderboard controller

--- application/controllers/api/Leaderboard.php
+++ application/controllers/api/Leaderboard.php
@@ -17,24 +17,52 @@ class Leaderboard extends CI_Controller
 
 
 	/**
-	 * Gets a leaderboard based on quiz id
-	 * 
-	 * @param   int  $quiz_id  Quiz ID for desired leaderboard
-	 * @return  JSON Object    A JSON object containing a leaderboard.
+	 * [getLeaderboard description]
+	 * @param  int $quiz_id [description]
+	 * @return [type]          [description]
 	 */
 	
 
-	public function getLeaderboardByQuizId($quiz_id)
+	public function getLeaderboard($quiz_id)
 	{
 		$json_data = json_decode(file_get_contents('php://input'), true);
 
 		if(is_numeric($json_data))
 		{
-			$output = json_encode($this->leaderboardModel->getLeaderboard($json_data));
+			$leaderboard = $this->leaderboardModel->getLeaderboard($json_data);
+			$stats       = [];
+			$count       = $this->leaderboardModel->getQuestionCount($json_data);
+
+			foreach($leaderboard->id as $leaderboard)
+			{
+				$user_quiz_id = $this->leaderboardModel->getStats($leaderboard->user_quiz_id);
+
+				$stats[$leaderboard->user_id] = $user_quiz_id;		
+			}
+
+			$output = json_encode([
+				'quiz_name' => $json_data,
+				'question_count' => $count,
+				'results' => $stats
+			]);
 
 			return $this->output->set_content_type('application/json')->set_content($output);
 		}
 
 		return false;
 	}
+
+	// public function getLeaderboardByQuizId($quiz_id)
+	// {
+	// 	$json_data = json_decode(file_get_contents('php://input'), true);
+
+	// 	if(is_numeric($json_data))
+	// 	{
+	// 		$output = json_encode($this->leaderboardModel->getLeaderboard($json_data));
+
+	// 		return $this->output->set_content_type('application/json')->set_content($output);
+	// 	}
+
+	// 	return false;
+	// }
 }
