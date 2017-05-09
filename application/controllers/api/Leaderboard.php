5c8b23dbe62be3f0f53220fa57ae2f652cbe3f9f
Lasse Lentz Thomsen <kontakt@lasselentz.dk>
2017-05-09 11:12:25 +0200

    Commit files

--- application/controllers/api/Leaderboard.php
+++ application/controllers/api/Leaderboard.php
@@ -5,5 +5,31 @@ class Leaderboard extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('Leaderboard_Model', 'leaderboardModel');
+	}
+
+	/**
+	 * Gets a leaderboard based on quiz id
+	 * 
+	 * @param  int    $quiz_id Quiz ID for desired leaderboard
+	 * @return [type]     [description]
+	 */
+	
+	public function getLeaderboardByQuizId(int $quiz_id)
+	{
+		if(is_numeric($quiz_id))
+		{
+			$this->leaderboardModel->
+		}
+	}
+
+	/**
+	 * Gets the
+	 * @param  int    $quiz_id [description]
+	 * @return [type]     [description]
+	 */
+	public function getFiveBestResults(int $quiz_id)
+	{
+
 	}
 }
