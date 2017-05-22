f2892c21f1dd64ebbdf3c0c7bb971b3c91869a95
Nicolaj Egelund <nicomanden@gmail.com>
2017-05-22 14:14:57 +0200

    Removed s

--- application/controllers/api/Quiz.php
+++ application/controllers/api/Quiz.php
@@ -209,7 +209,7 @@ class Quiz extends CI_Controller
 		}
 
 		$user_answer_ids = $this->db
-			->select('user_answers.id')
+			->select('user_answer.id')
 			->join('user_answer', 'user_answer.user_quiz_id = user_quiz.id')
 			->where('quiz_id', $id)
 			->get('user_quiz')
